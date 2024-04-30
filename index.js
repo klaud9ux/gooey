546e680812149c79746d02131736355683bed3f5
klaud9 <klaud9@userinsight.co.kr>
2024-04-30 14:53:01 +0900

    윈도우 사이즈 대응

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const canvas = document.querySelector('canvas')
-
-
 const ctx = canvas.getContext('2d')
 const dpr = window.devicePixelRatio
 
-const canvasWidth = innerWidth
-const canvasHeight = innerHeight
+let canvasWidth
+let canvasHeight
 
-canvas.style.width = canvasWidth + 'px'
-canvas.style.height = canvasHeight + 'px'
+const randomNumBetween = (min, max) => {
+  return Math.random() * (max - min + 1) + min
+}
+let particles
 
-canvas.width = canvasWidth * dpr
-canvas.height = canvasHeight * dpr
-ctx.scale(dpr, dpr)
+let interval = 1000 / 60
+let now, delta
+let then = Date.now()
 
 const feGaussianBlur = document.querySelector('feGaussianBlur')
 const feColorMatrix = document.querySelector('feColorMatrix')
@@ -23,12 +23,10 @@ const controls = new function(){
   this.alphaOffset = -23
   this.acc = 1.03
 }
-
 let gui = new dat.GUI()
 
 const f1 = gui.addFolder('Gooey Effect')
 f1.open()
-
 f1.add(controls, 'blurValue', 0, 100).onChange(value => {
   feGaussianBlur.setAttribute('stdDeviation', value)
 })
@@ -37,9 +35,6 @@ f1.add(controls, 'alphaChannel', 1, 200).onChange(value => {
 })
 f1.add(controls, 'alphaOffset', -40, 40).onChange(value => {
   feColorMatrix.setAttribute('values', `1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 ${controls.alphaChannel} ${value}`)
-})
-f1.add(controls, 'acc', 1, 1.5, 0.01).onChange(value => {
-  particles.forEach(particle => particle.acc = value)
 })
 const f2 = gui.addFolder('Particle Property')
 f2.open()
@@ -53,13 +48,13 @@ class Particle {
     this.y = y
     this.radius = radius
     this.vy = vy
-    this.acc = 1.02357
+    this.acc = 1.03
   }
-  update(){
+  update() {
     this.vy *= this.acc
     this.y += this.vy
   }
-  draw(){
+  draw() {
     ctx.beginPath()
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI / 180 * 360)
     ctx.fillStyle = 'orange'
@@ -67,29 +62,30 @@ class Particle {
     ctx.closePath()   
   }
 }
-const x = 100
-const y = 100
-const radius = 50
-const particle = new Particle(x, y, radius)
-const TOTAL = 24
-const randomNumBetween = (min, max) => {
-  return Math.random() * (max - min + 1) + min
+
+function init() {
+  canvasWidth = innerWidth
+  canvasHeight = innerHeight
+  
+  canvas.width = canvasWidth * dpr
+  canvas.height = canvasHeight * dpr
+  ctx.scale(dpr, dpr)
+
+  canvas.style.width = canvasWidth + 'px'
+  canvas.style.height = canvasHeight + 'px'
+
+  particles = []
+  const TOTAL = canvasWidth / 10
+
+  for (let i = 0; i < TOTAL; i++) {
+    const x = randomNumBetween(0, canvasWidth)
+    const y = randomNumBetween(0, canvasHeight)
+    const radius = randomNumBetween(16, 64)
+    const vy = randomNumBetween(1, 5)
+    const particle = new Particle(x, y, radius, vy)
+    particles.push(particle)
+  }
 }
-let particles = []
-
-for (let i = 0; i < TOTAL; i++){
-  const x = randomNumBetween(0, canvasWidth)
-  const y = randomNumBetween(0, canvasHeight)
-  const radius = randomNumBetween(16, 64)
-  const vy = randomNumBetween(1, 5)
-  const particle = new Particle(x, y, radius, vy)
-  particles.push(particle)
-}
-
-let interval = 1000 / 60
-let now, delta
-let then = Date.now()
-
 
 function animate() {
   window.requestAnimationFrame(animate)
@@ -114,4 +110,11 @@ function animate() {
 
   then = now - (delta % interval)
 }
-animate()
+window.addEventListener('load', () => {
+  init()
+  animate()
+})
+
+window.addEventListener('resize', () => {
+  init()
+})
