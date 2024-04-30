65634ab2512d109fc453893245476b7ed0bdabef
klaud9 <klaud9@userinsight.co.kr>
2024-04-30 14:01:34 +0900

    dat gui 라이브러리 추가

--- index.js
+++ index.js
@@ -14,14 +14,38 @@ canvas.width = canvasWidth * dpr
 canvas.height = canvasHeight * dpr
 ctx.scale(dpr, dpr)
 
-ctx.beginPath()
-ctx.arc(100, 100, 50, 0, Math.PI / 180 * 180)
-ctx.fillStyle = 'red'
-ctx.fill()
-ctx.closePath()
+const feGaussianBlur = document.querySelector('feGaussianBlur')
+const feColorMatrix = document.querySelector('feColorMatrix')
 
-//cavanas 에서는 degree가 아닌 radius
-//Math.PI * 180 = 1 deg
+const controls = new function(){
+  this.blurValue = 40
+  this.alphaChannel = 100
+  this.alphaOffset = -23
+  this.acc = 1.03
+}
+
+let gui = new dat.GUI()
+
+const f1 = gui.addFolder('Gooey Effect')
+f1.open()
+
+f1.add(controls, 'blurValue', 0, 100).onChange(value => {
+  feGaussianBlur.setAttribute('stdDeviation', value)
+})
+f1.add(controls, 'alphaChannel', 1, 200).onChange(value => {
+  feColorMatrix.setAttribute('values', `1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 ${value} ${controls.alphaOffset}`)
+})
+f1.add(controls, 'alphaOffset', -40, 40).onChange(value => {
+  feColorMatrix.setAttribute('values', `1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 ${controls.alphaChannel} ${value}`)
+})
+f1.add(controls, 'acc', 1, 1.5, 0.01).onChange(value => {
+  particles.forEach(particle => particle.acc = value)
+})
+const f2 = gui.addFolder('Particle Property')
+f2.open()
+f2.add(controls, 'acc', 1, 1.5, 0.01).onChange(value => {
+  particles.forEach(particle => particle.acc = value)
+})
 
 class Particle { 
   constructor(x, y, radius, vy) {
